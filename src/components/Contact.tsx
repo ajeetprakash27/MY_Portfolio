b16329b0c122f8f0d798e11197ddb67981a7562f
AJEET PRAKASH PANDEY <ajeetprpandey2701@gmail.com>
2025-10-05 11:11:55 +0530

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Card, CardContent } from "@/components/ui/card";
-import { Github, Linkedin, Mail, Phone } from "lucide-react";
+import { Github, Linkedin, Gmail, Phone } from "lucide-react";
 import { useToast } from "@/hooks/use-toast";
 
 const Contact = () => {
